3a863ec635edb97e3c6803cfe7fc9f5df3574eba
Stephen Feagin <stephenfeagin@gmail.com>
2019-11-04 14:21:10 -0500

    Ch 4: Database

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+"""app/routes.py"""
 from flask import flash, redirect, render_template, url_for
 
 from app import app
@@ -11,8 +12,8 @@ def index():
 
     user = {"username": "Miguel"}
     posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!",},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!",},
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
 
--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     """The config object stores configuration settings for the Flask app"""
 
     SECRET_KEY: str = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
